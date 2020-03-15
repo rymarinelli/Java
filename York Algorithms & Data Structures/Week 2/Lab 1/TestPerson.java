7695b0029b5d27511e6ece5b831918c3552ca174
Ryan Marinelli <46245460+rymarinelli@users.noreply.github.com>
2020-03-15 13:15:58 -0500

    Create TestPerson.java

---
+++ York Algorithms & Data Structures/Week 2/Lab 1/TestPerson.java
@@ -0,0 +1,11 @@
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// Create Person object and test the methods
+         Person test = new Person();
+         test.promptInfo();
+         test.displayProfile();
+	}
+
+}
