5a4f78da7c89e0539f848982da09a9f6476eac8e
Ryan Marinelli <46245460+rymarinelli@users.noreply.github.com>
2020-03-15 14:57:12 -0500

    Create TestMathDemo.java

---
+++ York Algorithms & Data Structures/Week 2/TestMathDemo.java
@@ -0,0 +1,11 @@
+
+public class TestMathDemo1 {
+
+	public static void main(String[] args) {
+		// Create MathDemo1 object and test the methods
+        MathDemo test = new MathDemo();
+        test.bestOfThree();
+       
+       	}
+
+}
