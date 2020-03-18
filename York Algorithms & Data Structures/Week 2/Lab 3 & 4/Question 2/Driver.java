7ad1621e59d257c86a6b3acbb7919e70b29739e3
Ryan Marinelli <46245460+rymarinelli@users.noreply.github.com>
2020-03-18 15:13:45 -0500

    Update Driver.java

--- York Algorithms & Data Structures/Week 2/Lab 3 & 4/Question 2/Driver.java
+++ York Algorithms & Data Structures/Week 2/Lab 3 & 4/Question 2/Driver.java
@@ -1,20 +1,24 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		Shopping test = new Shopping();
-		Scanner sc = new Scanner(System.in);
-		String item = sc.nextLine();
+		
 		
 		//test.setItem(sc.nextLine());
-		test.setItem(item);
-		test.addItem(item);
+		test.addItem("eggs");
+		test.addItem("cream");
+		test.addItem("fish");
 		
 		
+		System.out.print(test.numberOfItems());
+		System.out.println("\n" + test.showItem(2));
+		System.out.println("\n" + test.showItem(0));
 		
 		
+		System.out.println("\n\n" + "From the start of the list");
+	    test.listItems();
 
 	}
 
