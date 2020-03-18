3bc0d3d16bc4ec47255adbd30f87f7c7109ac7bd
Ryan Marinelli <46245460+rymarinelli@users.noreply.github.com>
2020-03-17 22:32:44 -0500

    Create Driver.java

---
+++ York Algorithms & Data Structures/Week 2/Lab 3 & 4/Driver.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.lang.*;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        BirdTable test = new BirdTable();
+        test.interactiveArray();
+        test.printHist();
+	}
+
+}
