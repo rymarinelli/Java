f7ffdd70b0a7c5671df7e414ae0e9edd07a984f4
Ryan Marinelli <46245460+rymarinelli@users.noreply.github.com>
2020-03-21 14:11:15 -0500

    Create TestDegreeGrader.java

---
+++ York Algorithms & Data Structures/Problem Set 1/TestDegreeGrader.java
@@ -0,0 +1,18 @@
+
+public class TestDegreeGrader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DegreeGrader test = new DegreeGrader(0,0,0,0);
+		//System.out.println(test.getModAverage());
+		//System.out.println(test.getISMAverage());
+		//System.out.println(test.getcompFailedCredits());
+		//System.out.println(test.getOutRightFailedMods());
+		
+		test.gradeDegree();
+		test.unitTest();
+	}
+	
+	
+
+}
